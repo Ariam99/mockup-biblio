827219436ace06e6d73ae12202d4b88bf5694164
Roberto Paredes <rdparedessalazar@gmail.com>
2015-07-17 10:49:54 -0500

    cambiar modal

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -3,6 +3,9 @@ var biblioApp = angular.module('biblioApp', ['ngDialog']);
 
 biblioApp.controller('MainCtrl', function($scope, ngDialog) {
 	$scope.clickToOpen = function () {
-		ngDialog.open({ template: 'popupTmpl' });
+		ngDialog.open({ 
+			template: 'popupTmpl',
+			showClose: false
+		});
 	};
 });
